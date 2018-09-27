f30fd76321101a2ca1bffe73a30af94c80e76d18
amanData <amanda.thouvenin@gmail.com>
2018-09-27 23:15:49 +0200

    added some art and cheese wheel button

---
+++ script.js
@@ -0,0 +1,13 @@
+var lacticMana = 0;
+
+
+function genLacticMana(x) {
+	lacticMana = lacticMana + x;
+	document.getElementById("lM").innerHTML = lacticMana;
+};
+
+
+
+/*var cheddarWheel = document.getElementById("cheddar");
+cheddarWheel.addEventListener ('click', genLacticMana(1));
+*/
