52a5171b6ba08de7e2c4b72c517b244f7b06c927
amanData <amanda.thouvenin@gmail.com>
2018-09-28 10:49:38 +0200

    added magic dairy school and runic cheddar wheel

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var lacticMana = 0;
+var fromana = 0;
 
 
-function genLacticMana(x) {
-	lacticMana = lacticMana + x;
-	document.getElementById("lM").innerHTML = lacticMana;
+function genFromana(x) {
+	fromana = fromana + x;
+	document.getElementById("fm").innerHTML = fromana;
 };
 
-
+function buyRuneWheel() {
+	document.getElementById('cheddar').src="images/cheese-aseprite/cheddar-rune.png"
+}
 
 /*var cheddarWheel = document.getElementById("cheddar");
 cheddarWheel.addEventListener ('click', genLacticMana(1));
 */
+
+
