d529a0aaf9d17a0cef57445dfd7bb42c1653730e
amanData <amanda.thouvenin@gmail.com>
2018-09-30 22:15:26 +0200

    changed some sprites

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 var fromana = 0;
+const fmDisplay = document.getElementById("fm");
 
+
+/* EDUCATION */
+var attendingMagicSchool = false;
+
+
+/* FUNCTIONS */
 
 function genFromana(x) {
 	fromana = fromana + x;
-	document.getElementById("fm").innerHTML = fromana;
+	fmDisplay.innerHTML = Math.floor(fromana);
 };
 
 function buyRuneWheel() {
-	document.getElementById('cheddar').src="images/cheese-aseprite/cheddar-rune.png"
+	if (fromana >= 10) {
+		document.getElementById('cheddar').src="images/cheese-aseprite/cheddar-rune.png";
+		fromana = fromana - 10;
+		fmDisplay.innerHTML = Math.floor(fromana);
+		document.getElementById('magicSchool').disabled = true;
+		attendingMagicSchool = true;
+	}
+	else {
+
+	}
+};
+
+
+
+
+
+/* SAVE */
+var save = {
+	fromana: fromana,
+	attendingMagicSchool: attendingMagicSchool
 }
 
-/*var cheddarWheel = document.getElementById("cheddar");
-cheddarWheel.addEventListener ('click', genLacticMana(1));
-*/
+/* GAME LOOP */
+window.setInterval(function(){
+	if (attendingMagicSchool == true){
+		genFromana(0.1)
+	}
+	
+
+	fmDisplay.innerHTML = Math.floor(fromana);
+}, 1000);
+
 
 
