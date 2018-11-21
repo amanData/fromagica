ebfd81158a4a67f57aed986ea5522237f348e59a
amanData <amanda.thouvenin@gmail.com>
2018-11-21 22:01:11 +0100

    when clicked the fromana bottle drops a rune in the inventory

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ var fromana = 0;
 const fmDisplay = document.getElementById("fm");
 
 
-/* EDUCATION */
+/*
+##################################################
+			EDUCATION
+##################################################
+*/
 var attendingMagicSchool = false;
 
 
-/* FUNCTIONS */
+/*
+##################################################
+			FUNCTIONS
+##################################################
+*/
 
+// prettify the numbers
 function prettify(input){
     var output = Math.round(input * 1000000)/1000000;
 	return output;
 }
 
+
+// general function to call when you generate some fromana and need to display input
 function genFromana(x) {
 	fromana = fromana + x;
 	fmDisplay.innerHTML = prettify(fromana);
 };
 
+// add a rune on the classic cheddar wheel and start generating fromana iddly
 function buyRuneWheel() {
 	if (fromana >= 24) {
 		document.getElementById('cheddar').src="images/cheese-aseprite/cheddar-rune.png";
@@ -26,27 +38,44 @@ function buyRuneWheel() {
 		document.getElementById('magicSchool').disabled = true;
 		attendingMagicSchool = true;
 	}
-	else {
-
-	}
 };
 
+/*
+##################################################
+			MANIPULATION
+##################################################
+*/
+// Dropping a rune in the inventory when the fromana bottle is clicked
+var freeSpace = document.getElementsByClassName('notFull');
+var fromanaBottle = document.getElementById('fromanaBottle');
+fromanaBottle.addEventListener("click", function() {
+		freeSpace.item(0).style="background: #fff";
+		freeSpace.item(0).classList.add('full');
+		freeSpace.item(0).classList.remove("notFull");
+	});
 
 
 
 
-/* SAVE */
+/*
+##################################################
+			SAVE
+##################################################
+*/
 var save = {
 	fromana: fromana,
 	attendingMagicSchool: attendingMagicSchool
 }
 
-/* GAME LOOP */
+/*
+##################################################
+			GAME LOOP
+##################################################
+*/
 window.setInterval(function(){
 	if (attendingMagicSchool == true){
 		genFromana(0.1)
 	}
-
 
 	fmDisplay.innerHTML = prettify(fromana);
 }, 1000);
